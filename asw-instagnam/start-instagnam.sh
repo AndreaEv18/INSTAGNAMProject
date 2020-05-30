dae2d50e98bf7126295eee7875e466acbb3324ef
marcodg97 <marcodegiovanni@live.com>
2020-05-30 23:50:47 +0200

    script rifiniti

--- asw-instagnam/start-instagnam.sh
+++ asw-instagnam/start-instagnam.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+if [ "$USER" != "root" ]; then
+    echo "E' necessario essere l'utente root per eseguire questo script"
+    exit 1
+fi
+
 gradle build &&
-docker-compose up
+docker-compose up -d
